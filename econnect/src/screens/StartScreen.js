00107b4f46ba8a82e31f83dfe982c8e48748e4e0
mogg22 <ahrthdus1@gmail.com>
2024-08-21 16:34:53 +0900

    🔧 Feat : StartScreen.js에 메인페이지 이동 버튼 임시 추가
    메인페이지 이동 버튼 임시 추가

--- econnect/src/screens/StartScreen.js
+++ econnect/src/screens/StartScreen.js
@@ -6,14 +6,9 @@ const StartScreen = ({ navigation }) => {
   return (
     <S.StyledSafeAreaView>
       <Text>시작 페이지</Text>
-      <Button
-        title="로그인"
-        onPress={() => navigation.navigate('Login')}
-      />
-      <Button
-        title="회원가입"
-        onPress={() => navigation.navigate('Signup')}
-      />
+      <Button title="로그인" onPress={() => navigation.navigate('Login')}/>
+      <Button title="회원가입" onPress={() => navigation.navigate('Signup')}/>
+      <Button title="메인페이지" onPress={() => navigation.navigate('Main')}/>
     </S.StyledSafeAreaView>
   );
 };
