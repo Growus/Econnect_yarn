c5490bfc31e20fc3eee34ade7c89617088258f38
pookey1104 <pookey1104@naver.com>
2024-09-02 03:11:09 +0900

    ✨ Feat: 회원 정보 수정, 비밀번호 변경 퍼블리싱 (#6)
    회원 정보 수정, 비밀번호 변경 퍼블리싱 (+ 갤러리에서 이미지 선택)

--- econnect/src/styles/ChangePWStyle.js
+++ econnect/src/styles/ChangePWStyle.js
@@ -1,5 +1,25 @@
 import styled from 'styled-components/native';
+import palette from '../lib/styles/colorPalette';
 
-export const Screen = styled.View``;
+export const StyledSafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: ${palette.backGreen};
+`;
 
-export const Text = styled.Text``;
+export const InputGap = styled.View`
+  width: 100%;
+  height: 283px;
+  padding: 0 33px;
+  margin-top: 50px;
+  gap: 32px;
+`;
+
+export const ButtonGap = styled.View`
+  width: 100%;
+  padding: 0 33px;
+  align-items: center;
+  position: absolute;
+  bottom: 80px;
+`;
--- econnect/src/styles/ProfileStyle.js
+++ econnect/src/styles/ProfileStyle.js
@@ -1,5 +1,51 @@
 import styled from 'styled-components/native';
+import palette from '../lib/styles/colorPalette';
 
-export const Screen = styled.View``;
+import Camera from '../assets/img/profile_camera.svg';
 
-export const Text = styled.Text``;
+export const StyledSafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: ${palette.backGreen};
+`;
+
+export const ProfileImg = styled.Image`
+  width: 118px;
+  height: 118px;
+  border-radius: 59px;
+  overflow: hidden;
+  margin-top: 35px;
+`;
+
+export const ProfileBtn = styled.Pressable.attrs({
+  shadowColor: 'rgb(0,0,0)',
+  shadowOffset: {width: 0, height: 0},
+  shadowOpacity: 0.25,
+  shadowRadius: 5,
+  elevation: 5,
+})`
+  margin-top: -30px;
+  margin-right: -95px;
+  margin-bottom: 38px;
+`;
+
+export const ProfileCamera = styled(Camera)`
+  width: 23px;
+  height: 23px;
+`;
+
+export const InputGap = styled.View`
+  width: 100%;
+  height: 184px;
+  padding: 0 33px;
+  gap: 38px;
+`;
+
+export const ButtonGap = styled.View`
+  width: 100%;
+  padding: 0 33px;
+  align-items: center;
+  position: absolute;
+  bottom: 80px;
+`;
